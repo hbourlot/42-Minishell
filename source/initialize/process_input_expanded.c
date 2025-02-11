ec9a88244d82e7417adcf148f5d50955935bbace
Jorge  Marquez <joralves@student.42lisboa.com>
2025-02-11 21:28:08 +0000

    Fix: Env -i cases, here_doc, signal.c

--- source/initialize/process_input_expanded.c
+++ source/initialize/process_input_expanded.c
@@ -3,55 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   process_input_expanded.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbourlot <hbourlot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joralves <joralves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:15:08 by joralves          #+#    #+#             */
-/*   Updated: 2025/02/11 18:32:38 by hbourlot         ###   ########.fr       */
+/*   Updated: 2025/02/11 21:07:40 by joralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-static void	iter_special_replace(t_cmd *command)
-{
-	int 	i;
-	char 	*ptr;
-
-	i = 0;
-	while (command->input_expanded[i] && command->input_expanded[i] == REP_SPACE)
-		i++;
-	if (command->input_expanded[i] == REP_SQ || command->input_expanded[i] == REP_DQ)
-	{
-		ptr = ft_strchr(command->input_expanded, REP_DQ);
-		if (ptr && ptr[1] != REP_DQ)
-			truncate_character(command->input_expanded, 2);
-		ptr = ft_strchr(command->input_expanded, REP_SQ);
-		if (ptr && ptr[1] != REP_SQ)
-			truncate_character(command->input_expanded, 1);
-		return;
-	}
-}
-
-
 char	**process_command_input_expanded(t_cmd *command)
 {
 	char	**cmd_args;
-	char	*ptr;
+	int		i;
 
-	(void)ptr;
+	i = 0;
 	identify_and_replace_sqpa_tokens(command->input_expanded);
 	replace_characters(command->input_expanded, REP_PIPE, '|');
 	replace_characters(command->input_expanded, REP_AND, '&');
-	iter_special_replace(command);
-	// ptr = ft_strchr(command->input_expanded, REP_DQ);
-	// if (ptr && ptr[1] != REP_DQ)
-	// 	truncate_character(command->input_expanded, 2);
-	// ptr = ft_strchr(command->input_expanded, REP_SQ);
-	// if (ptr && ptr[1] != REP_SQ)
-	// 	truncate_character(command->input_expanded, 1);
 	cmd_args = ft_split(command->input_expanded, REP_SPACE);
 	if (!cmd_args && command->settings.expansion == false)
 		handle_error(E_MALLOC, NULL, __func__);
+	while (cmd_args[i])
+	{
+		truncate_character(cmd_args[i], 1);
+		truncate_character(cmd_args[i], 2);
+		i++;
+	}
 	return (cmd_args);
 }
